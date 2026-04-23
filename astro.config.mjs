1364cba52ab45e6ed4e51befb6b08c7a4b8f474f
베르 <club20608@gmail.com>
2026-04-23 15:38:56 +0900

    fix: set github pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 const site = process.env.SITE || 'https://kkabeming-world.github.io';
+const base = process.env.BASE_PATH || '/blog';
 
 // https://astro.build/config
 export default defineConfig({
 	site,
+	base,
 	integrations: [mdx(), sitemap()],
 });
